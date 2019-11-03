ec46bdc80893a47ef35a1b229c338b145e96be75
Michael Dermksian <mdermksian@gmail.com>
2019-11-03 14:25:47 -0500

    More work on ps10

--- PS10-mdermksi/ps10.cpp
+++ PS10-mdermksi/ps10.cpp
@@ -7,7 +7,10 @@ private:
 protected:
     char *GetPointer();
     const char *GetPointer() const;
+    const char *c_str() const;
     void resize(long long int newLen);
+    long long int size(void) const;
+    void push_back(char c);
 public:
     TextBase();
     ~TextBase();
@@ -20,6 +23,17 @@ char *TextBase::GetPointer(){
 
 const char *TextBase::GetPointer() const {
     return str;
+}
+
+const char *TextBase::c_str() const {
+    if(nullptr!=GetPointer())
+	{
+		return GetPointer();
+	}
+	else
+	{
+		return "";
+	}
 }
 
 TextBase::TextBase(){
@@ -48,6 +62,22 @@ void TextBase::resize(long long int newLen) {
     str = newStr;
 }
 
+long long int TextBase::size(void) const{
+    long long int l = 0;
+    auto *ptr = c_str();
+    for(l=0; ptr[l]!=0; ++l){}
+    return l;
+}
+
+void TextBase::push_back(char c){
+    long long int l = size();
+    long long int newL = l+1;
+    resize(newL+1);
+    auto s=GetPointer();
+    s[l] = c;
+    s[newL] = 0;
+}
+
 class BowlScore : public TextBase {
 private:
     int score;
@@ -55,13 +85,12 @@ public:
     BowlScore();
     ~BowlScore();
 
-    const char *getScore(FILE *fp);
+    const char *inputGameString(FILE *fp);
+    void CalculateScore(void);
 
-    void set(const char str[]);
-    void push_back(char c);
-    void pop_back(void);
-    const char *c_str(void) const;
-    long long int size(void) const;
+    const char *getGameString(void);
+    const int getScore(void);
+    void setScore(int score);
 };
 
 BowlScore::BowlScore(){
@@ -70,17 +99,33 @@ BowlScore::~BowlScore(){
     clear();
 }
 
-const char *BowlScore::getScore(FILE *fp){
+const char *BowlScore::inputGameString(FILE *fp){
     clear();
-
+    printf("Enter Frames> ");
     char buf[22];
-    while (nullptr!=::fgets(buf,21,fp))
+    while (nullptr!=fgets(buf,21,fp))
     {
-        
+        for(int i = 0; 0!=buf[i]; ++i){
+            if(buf[i] == '\n'){
+                return c_str();
+            }
+            push_back(buf[i]);
+        }
+        if(0<size()){
+            return GetPointer();
+        }
+        return nullptr;
     }
-    
+}
+
+const char *BowlScore::getGameString(void){
+    return c_str();
 }
 
 int main(void) {
+    BowlScore bs;
 
+    bs.inputGameString(stdin);
+
+    printf("%s", bs.getGameString());
 }
